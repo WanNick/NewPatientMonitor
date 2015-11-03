56083b4a240cbdce4a43598b1c9827576f797278
Jack <jack.w.sanger@gmail.com>
2015-11-03 20:34:41 +0000

    Added monitor class with unit tests

---
+++ NewPatientMonitor/NewPatientMonitor/NewPatientMonitorTest/Module_Tester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewPatientMonitor;
+
+/*
+ *  Unit tests for the module class
+*/ 
+
+namespace NewPatientMonitorTest
+{
+    [TestClass]
+    public class Module_Tester
+    {
+        [TestMethod]
+        public void DefaultModuleCreated()
+        {
+            Module testModule = new Module();
+
+            Assert.AreEqual(testModule.Name, NewPatientMonitor.DefaultSettings.NEW_MODULE_NAME);
+            Assert.AreEqual(testModule.LowerLimit, NewPatientMonitor.DefaultSettings.LOWER_DEFAULT_VALUE);
+            Assert.AreEqual(testModule.UpperLimit, NewPatientMonitor.DefaultSettings.UPPER_DEFAULT_VALUE);
+        }
+    }
+}
