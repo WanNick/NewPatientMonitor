f5d8115cf4103d05490edbcc7defa91cfc7cf485
WanNick <nickwan@hotmail.co.uk>
2015-11-05 10:20:20 +0000

    Comments added to TestAlarmTester

--- NewPatientMonitor/NewPatientMonitor/NewPatientMonitorTest/TestAlarmTester.cs
+++ NewPatientMonitor/NewPatientMonitor/NewPatientMonitorTest/TestAlarmTester.cs
@@ -23,6 +23,9 @@ namespace NewPatientMonitorTest
         [TestMethod]
         public void IsAlarmTestGoodCreation()
         {
+            // This test method test whether the created alarm is the same as the one setup for the test initialize above.
+            // If the elements below this comment is the same as the one created the test will pass, else the test will fail.
+
             Assert.AreEqual("Module Name", alarmTesterCreated.NameOfAlarm);
             Assert.AreEqual(12f, alarmTesterCreated.LowerLimit);
             Assert.AreEqual(54f, alarmTesterCreated.UpperLimit);
@@ -31,6 +34,9 @@ namespace NewPatientMonitorTest
         [TestMethod]
         public void IsAlarmWithinLimits()
         {
+            // This test method will test whether the alarm values are in between the values 12f and 54f. 
+            // If the values fall in between 12 and 54 the test should pass.
+
             Assert.IsFalse(alarmTesterCreated.ValueOutsideLimits (13f));
             Assert.IsFalse(alarmTesterCreated.ValueOutsideLimits (53f));
             Assert.IsFalse(alarmTesterCreated.ValueOutsideLimits(41f));
@@ -40,6 +46,9 @@ namespace NewPatientMonitorTest
         [TestMethod]
         public void IsAlarmOutsideLimits()
         {
+            // This test method will test whether the values are outside the setup values 12f and 54f.
+            // If they are outside the test should pass. If in between should fail.
+
             Assert.IsTrue(alarmTesterCreated.ValueOutsideLimits(11f));
             Assert.IsTrue(alarmTesterCreated.ValueOutsideLimits(9f));
             Assert.IsTrue(alarmTesterCreated.ValueOutsideLimits(65f));
@@ -49,6 +58,8 @@ namespace NewPatientMonitorTest
         public void  AlarmDropsToZero() 
         {
             // Do we need to check whether the values drop to zero.
+
+            // This test should fail by default, since the values are greater than 0 (Both 12f and 54f are > 0)
 
             Assert.IsFalse(alarmTesterCreated.ValueOutsideLimits (0f));
             Assert.IsFalse(alarmTesterCreated.ValueOutsideLimits(0f));
